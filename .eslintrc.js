193a2ad0f9eb7496f4cb1550340db64e45cfd7b0
Maxim <maxim.rogozneak@gmail.com>
2022-11-05 17:05:18 +0200

    jest configured

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,6 +2,7 @@ module.exports = {
     env: {
         browser: true,
         es2021: true,
+        jest: true,
     },
     extends: [
         'plugin:react/recommended',
@@ -43,6 +44,7 @@ module.exports = {
         }],
         'max-len': ['error', {
             code: 150,
+            ignoreComments: true,
         }],
     },
     globals: {
