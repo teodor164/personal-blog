3bba42ba17b60eb9800dd67d39c16bd5f4106cf7
MaximRogozneac <maxim.rogozneak@webmarc.io>
2023-02-04 17:54:17 +0200

    storybook path fixed

--- config/storybook/webpack.config.ts
+++ config/storybook/webpack.config.ts
@@ -11,7 +11,7 @@ export default ({ config }: {config: webpack.Configuration}) => {
         html: '',
     };
 
-    config!.resolve!.modules!.push(paths.src);
+    config!.resolve!.modules!.unshift(paths.src);
     config!.resolve!.extensions!.push('.ts', 'tsx');
 
     // eslint-disable-next-line no-param-reassign
