7b02dd9bf577364c32b3030e672dd25e070455b7
MaximRogozneac <maxim.rogozneak@webmarc.io>
2023-10-08 23:13:05 +0300

    add fixtures

--- cypress/e2e/article/articles-list.cy.ts
+++ cypress/e2e/article/articles-list.cy.ts
@@ -11,4 +11,11 @@ describe('Article list page', () => {
         cy.getByTestId('article-list').should('exist');
         cy.getByTestId('article-list-item').should('have.length.greaterThan', 3);
     });
+
+    it('Article successfully loaded (on fixtures)', () => {
+        cy.intercept('GET', '**/articles?*', { fixture: 'articles.json' });
+
+        cy.getByTestId('article-list').should('exist');
+        cy.getByTestId('article-list-item').should('have.length.greaterThan', 3);
+    });
 });
